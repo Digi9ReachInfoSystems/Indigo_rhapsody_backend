1d1a2c5cefb25ee5d76903da9c56ab414d6727ad
Flutterneu <rsmx141@gmail.com>
2025-05-20 12:01:32 +0530

    Fix route definition for searchUser in couponRoutes.js

--- src/routes/couponRoutes.js
+++ src/routes/couponRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const couponController = require("../controllers/couponController");
-
+router.get("/searchUser", couponController.searchUsers);
 router.post("/", couponController.createCoupon);
 router.get("/", couponController.getAllCoupons);
 // router.get("/getall", couponController.getAllCouponsAll);
 router.get("/:id", couponController.getCouponById);
-router.get("/searchUser", couponController.searchUsers);
+
 router.put("/:id", couponController.updateCoupon);
 router.delete("/:id", couponController.deleteCoupon);
 router.post("/applyCoupon", couponController.applyCouponToCart);
