7c73f70bfef663ddd13eff0755a7fea0429197ce
svecchaApps <sveccha.apps@gmail.com>
2025-06-02 14:23:23 +0530

    Update contentVIdeosModel.js

--- src/models/contentVIdeosModel.js
+++ src/models/contentVIdeosModel.js
@@ -1,46 +1,59 @@
 const mongoose = require("mongoose");
 
-const contentVideoSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    // required: true,
-  },
-  comments: [
-    {
-      type: String,
+const contentVideoSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-  ],
-  creatorId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Video",
-    // required: true,
+    comments: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        commentText: String,
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    createdDate: {
+      type: Date,
+      default: Date.now,
+    },
+    no_of_likes: {
+      type: Number,
+      default: 0,
+    },
+    no_of_Shares: {
+      type: Number,
+      default: 0,
+    },
+    likedBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    is_approved: {
+      type: Boolean,
+      default: false,
+    },
   },
-  priceL: {
-    type: Number,
-   
-  },
-  videoUrl: {
-    type: String,
-    // required: true,
-  },
-  createdDate: {
-    type: Date,
-    default: Date.now,
-  },
-  no_of_likes: {
-    type: Number,
-    default: 0,
-  },
-  no_of_Shares: {
-    type: Number,
-    default: 0,
-  },
-  likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  is_approved: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("ContentVideo", contentVideoSchema);
+const ContentVideo = mongoose.model("ContentVideo", contentVideoSchema);
+
+module.exports = ContentVideo;
